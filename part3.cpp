dc3a6b40acc1cd44ca444bc32d73ec5063fc5612
rishi1001 <rishi.shah10122001@gmail.com>
2021-03-24 18:53:12 +0530

    Changes done

--- part3.cpp
+++ part3.cpp
@@ -17,6 +17,7 @@ vector<double> baseline_dynamic;
 int frame_number=0;
 int resolve=1;
 int x=5;
+bool print_data=false;
 
 void CallBackFunc(int event,int x,int y,int flags,void* userdata) {
     // Store the coordinates of corners of the road
@@ -73,31 +74,7 @@ double calcDiff(Mat img1, Mat img2) {
     return sum(delta)[0];
 }
 
-void printData(vector<int> frames, vector<double> &queue, vector<double> &dynamic) {
-    // Normalise and print data
 
-    double ma = 0;
-    double mi = queue[0];
-    for(double e : queue) {
-        ma = max(ma, e);
-        mi= min(mi,e);
-    }
-    for(double &e: queue) {
-        e = (e-mi)/(ma-mi);
-    }
-    ma = 0;
-    mi = dynamic[0];
-    for(double e : dynamic) {
-        ma = max(ma, e);
-        mi= min(mi,e);
-    }
-    for(double &e: dynamic) {
-        e = (e-mi)/(ma-mi);
-    }
-
-    cout << "Frame,Queue,Moving\n";
-    for(int i = 0; i < frames.size(); i++) cout << frames[i] << "," << queue[i] << "," << dynamic[i] << "\n";
-}
 
 
 void get_frames(string video){
@@ -153,25 +130,116 @@ void get_density(){
     }
 }
 
-void get_baseline(){                                   // get baseline queue and dyn vector from csv
+void get_baseline(string file){                                   // get baseline queue and dyn vector from csv
+    fstream fin;
+  
+    fin.open(file, ios::in);
+  
+    int count = 0;
+  
+    vector<string> row;
+    string line, word, temp;
+  
+    while (fin >> temp) {
+  
+        row.clear();
+  
+        // read an entire row and
+        // store it in a string variable 'line'
+        getline(fin, line);
+  
+        // used for breaking words
+        stringstream s(line);
+  
+        // read every column data of a row and
+        // store it in a string variable, 'word'
+        while (getline(s, word, ', ')) {
+  
+            // add all the column data
+            // of a row to a vector
+            row.push_back(word);
+        }
 
+        count++;
+        if (count==1) continue;
+        baseline_queue.push_back(stoi(row[1]));
+        baseline_dynamic.push_back(stoi(row[2]));
+
+    }
+    
 }
 
 void utility_cal(){
-    double utility_queue=0,utility_dynamic=0;
+    double utility_queue=0,utility_dynamic=0;                                     // should we normalise the data for queue and density like in baseline?
     for(int i=0;i<frame_number;i++){
         utility_queue+=((queue[i]-baseline_queue[i])*(queue[i]-baseline_queue[i]))/baseline_queue[i];        // is this division good?
         utility_dynamic+=((dynamic[i]-baseline_dynamic[i])*(dynamic[i]-baseline_dynamic[i]))/baseline_dynamic[i];
     }    
     utility_queue=utility_queue/frame_number;
     utility_dynamic=utility_dynamic/frame_number;
-        
+
+    cout << "Utility of queue is : " << fixed << utility_queue << setprecision(5);
+    cout<<"\n";
+    cout << "Utility of dynamic is : " << fixed << utility_dynamic << setprecision(5);
+    cout<<"\n";
+
+}
+
+void printData() {
+    // Normalise and print data
+
+    double ma = 0;
+    double mi = queue[0];
+    for(double e : queue) {
+        ma = max(ma, e);
+        mi= min(mi,e);
+    }
+    for(double &e: queue) {
+        e = (e-mi)/(ma-mi);
+    }
+    ma = 0;
+    mi = dynamic[0];
+    for(double e : dynamic) {
+        ma = max(ma, e);
+        mi= min(mi,e);
+    }
+    for(double &e: dynamic) {
+        e = (e-mi)/(ma-mi);
+    }
+
+    cout << "Frame,Queue,Moving\n";
+    for(int i = 0; i < frames.size(); i++) cout << i << "," << queue[i] << "," << dynamic[i] << "\n";
 }
 
 
+
 int main(int argc, char* argv[]) {
+
+    time_t start, end;
+    time(&start);
+
+    if(argc < 3) {
+        cout << "Error: No input video and/or image provided\n Please execute as ./part3 <video> <empty_frame>\n";
+        exit(1);
+    } else if(argc > 8) {
+        cout << "Warning: More than three arguments provided\n";             // ./part3 <video> <empty_frame> xrd x r d baseline.csv     
+    }
+
+    get_baseline(argv[argc-1]);
+
     
-                                                    // take resolve and x also as arguement
+    if (argc>=4){
+        string arg=argv[3];
+        if (arg=="x") x=stoi(argv[4]);
+        else if (arg=="r") resolve=stoi(argv[4]);
+        else if (arg=="d") print_data=true;
+        else if (arg=="xr") x=stoi(argv[4]),resolve=stoi(argv[5]);
+        else if (arg=="xd") x=stoi(argv[4]),print_data=true;
+        else if (arg=="rd") resolve=stoi(argv[4]),print_data=true;
+        else if (arg=="xrd") x=stoi(argv[3]),resolve=stoi(argv[4]),print_data=true;
+    }
+
+    Mat im_src = imread(argv[2]);
     if(im_src.empty()) {
         cout << "Error reading image " << argv[2] << "\n";
         exit(1);
@@ -185,5 +253,12 @@ int main(int argc, char* argv[]) {
     get_density();
 
     utility_cal();
+
+    if (print_data) printData();
+
+    time(&end);
+    double time_taken = double(end - start);
+    cout << "Time taken by program is : " << fixed << time_taken << setprecision(5);
+    cout << " sec " << endl;
 
 }
